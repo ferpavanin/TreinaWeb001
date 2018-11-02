d14305cb2705e864f95239ab74609feaec046529
ferpavanin <ferpavanin@hotmail.com>
2018-11-02 18:38:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/frp/treinaweb001/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.frp.treinaweb001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton imgBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inicializaComp();
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Fernando", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void inicializaComp(){
+        imgBtn = findViewById(R.id.imageButton);
+    }
+
+}
